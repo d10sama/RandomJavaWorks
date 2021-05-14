357c616cacdfa2ccbe47796621c2dc0846700775
d10sama <d10sama@bupt.edu.cn>
2021-05-14 15:42:50 +0800

    Create courtfield success

--- src/pro1_sopgomore_javaweb_works/CourtField.java
+++ src/pro1_sopgomore_javaweb_works/CourtField.java
@@ -26,9 +26,11 @@ public class CourtField extends Application
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		VBox Field =new VBox(0);
-		CFmid cm1=new CFmid();
-		cm1.setAlignment(Pos.CENTER);
-		Field.getChildren().add(cm1);
+		CFcorner cm1=new CFcorner(0);
+		CFcorner cm2=new CFcorner(1);
+		CFcorner cm3=new CFcorner(2);
+		CFcorner cm4=new CFcorner(3);
+		Field.getChildren().addAll(cm1,cm2,cm3,cm4);
 		
 		Scene scene = new Scene(Field,1024,768);
 		primaryStage.setScene(scene);
@@ -40,76 +42,172 @@ public class CourtField extends Application
 		launch(args);
 	}
 }
+
+//静态类
+//若有全局方法及变量则写入其中
 class Paint{
 	
 }
+
 class CFmid extends Label{
 	String path;
+	String path_W;
+	String path_B;
 	Image i1;
 	ImageView i1v;
-	CFmid() throws FileNotFoundException
+	CFmid()
 	{
 		path=new File("lib/MidField.png").getAbsolutePath();
-		Image image = new 
-				Image(new FileInputStream(path));
-		i1v=new ImageView(image);
-		i1v.setPreserveRatio(true);
-		this.setText("114514");
-		this.setPrefSize(200, 200);
-	    this.setGraphic(i1v);
+		path_W=new File("lib/MidField_W.png").getAbsolutePath();
+		path_B=new File("lib/MidField_B.png").getAbsolutePath();
+		try {
+			i1 = new 
+					Image(new FileInputStream(path));
+			}catch(Exception e) {e.printStackTrace();}
+			finally {
+			i1v=new ImageView(i1);
+			i1v.setPreserveRatio(true);
+			this.setPrefSize(50, 50);
+		    this.setGraphic(i1v);
+			}
 	}
+	//0变白，1变黑
 	void Turn(int i)
 	{
+		try {	
+			if(i==0)
+			{	
+				i1=new Image(
+						new FileInputStream(path_W));
+				i1v=new ImageView(i1);
+				i1v.setPreserveRatio(true);
+				this.setPrefSize(50, 50);
+			    this.setGraphic(i1v);
+			}
+			else
+			{
+				i1=new Image(
+						new FileInputStream(path_B));
+				i1v=new ImageView(i1);
+				i1v.setPreserveRatio(true);
+				this.setPrefSize(50, 50);
+			    this.setGraphic(i1v);
+			}
+		}catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
 class CFside extends Label{
-	
-	CFside()
+	String path;
+	String path_W;
+	String path_B;
+	Image i1;
+	ImageView i1v;
+	int iRotated;
+	CFside(int i)
 	{
-		
+		path=new File("lib/SideField.png").getAbsolutePath();
+		path_W=new File("lib/SideField_W.png").getAbsolutePath();
+		path_B=new File("lib/SideField_B.png").getAbsolutePath();
+		try {
+			i1 = new 
+					Image(new FileInputStream(path));
+			}catch(Exception e) {e.printStackTrace();}
+			finally {
+			i1v=new ImageView(i1);
+			i1v.setPreserveRatio(true);
+			this.setPrefSize(50, 50);
+			iRotated=i*90;
+			i1v.setRotate(iRotated);
+		    this.setGraphic(i1v);
+			}
 	}
+	//0变白，1变黑
 	void Turn(int i)
 	{
-		
-	}
-	void Rotate(int i)
-	{
-		switch(i)
+		try {	
+			if(i==0)
+			{	
+				i1=new Image(
+						new FileInputStream(path_W));
+				i1v=new ImageView(i1);
+				i1v.setPreserveRatio(true);
+				this.setPrefSize(50, 50);
+				i1v.setRotate(iRotated);
+			    this.setGraphic(i1v);
+			}
+			else
+			{
+				i1=new Image(
+						new FileInputStream(path_B));
+				i1v=new ImageView(i1);
+				i1v.setPreserveRatio(true);
+				this.setPrefSize(50, 50);
+				i1v.setRotate(iRotated);
+			    this.setGraphic(i1v);
+			}
+		}catch(FileNotFoundException e)
 		{
-			case 1:
-				this.Rotate(90);
-				break;
-			case 2:this.Rotate(180);
-				break;
-			case 3:this.Rotate(270);
-				break;
+			e.printStackTrace();
 		}
 	}
 }
 class CFcorner extends Label{
-	CFcorner()
+	String path;
+	String path_W;
+	String path_B;
+	Image i1;
+	ImageView i1v;
+	int iRotated;
+	CFcorner(int i)
 	{
-		this.setGraphic(new ImageView());
-	}
-	void Turn(int i)
-	{
-		if(i==0)
-			this.setGraphic(new ImageView());
-		else
-			this.setGraphic(new ImageView());
-	}
-	void Rotate(int i)
-	{
-		switch(i)
-		{
-			case 1:
-				this.Rotate(90);
-				break;
-			case 2:this.Rotate(180);
-				break;
-			case 3:this.Rotate(270);
-				break;
+		path=new File("lib/CornerField.png").getAbsolutePath();
+		path_W=new File("lib/CornerField_W.png").getAbsolutePath();
+		path_B=new File("lib/CornerField_B.png").getAbsolutePath();
+		try {
+		i1 = new 
+				Image(new FileInputStream(path));
+		}catch(Exception e) {e.printStackTrace();}
+		finally {
+		i1v=new ImageView(i1);
+		i1v.setPreserveRatio(true);
+		iRotated=i*90;
+		i1v.setRotate(iRotated);
+		this.setPrefSize(50, 50);
+	    this.setGraphic(i1v);
 		}
 	}
+	//0变白，1变黑
+	void Turn(int i)
+	{
+		try {	
+			if(i==0)
+			{	
+				i1=new Image(
+						new FileInputStream(path_W));
+				i1v=new ImageView(i1);
+				i1v.setPreserveRatio(true);
+				i1v.setRotate(iRotated);
+				this.setPrefSize(50, 50);
+			    this.setGraphic(i1v);
+			}
+			else
+			{
+				i1=new Image(
+						new FileInputStream(path_B));
+				i1v=new ImageView(i1);
+				i1v.setPreserveRatio(true);
+				i1v.setRotate(iRotated);
+				this.setPrefSize(50, 50);
+			    this.setGraphic(i1v);
+			}
+		}catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 }
 
--- src/pro1_sopgomore_javaweb_works/Envs.java
+++ src/pro1_sopgomore_javaweb_works/Envs.java
@@ -11,30 +11,74 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Envs extends Application{
+	CFmid[] mids;
+	CFside[] top;
+	CFside[] left;
+	CFside[] rig;
+	CFside[] bottom;
+	CFcorner ul,ur,dl,dr;
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		GridPane Field=new GridPane();
-		Field.setAlignment(Pos.CENTER);
-		Field.setVgap(0);
-		Field.setHgap(0);
-		Field.setHgap(20);
-		Field.setVgap(20);
-		CFmid[] b=new CFmid[400];
-		for(int i=0;i<20;i++)
-			for(int j=0;j<20;j++)
-		{	b[i*j]=new CFmid();
-			b[i*j].setPrefSize(20, 20);
-			b[i*j].setText(String.format("%d", i*j));
-			Field.add(b[i*j],i,j);
-		}
+		GridPane cf=new GridPane();
+		CourtField(cf);
 		
-		Scene scene = new Scene(Field,1024,768);
+		Scene scene = new Scene(cf,1324,800);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("五子棋");
 		primaryStage.show();
 	}
 	public static void main(String[] args) {
 	    launch(args);
+	}
+	void CourtField(GridPane cf)
+	{
+		int PosDetect=0;
+		cf.setAlignment(Pos.TOP_CENTER);
+		ul=new CFcorner(0);
+		mids=new CFmid[234];
+		top=new CFside[18];
+		left=new CFside[13];
+		rig=new CFside[13];
+		bottom=new CFside[18];
+		ur=new CFcorner(1);
+		dl=new CFcorner(3);
+		dr=new CFcorner(2);
+		cf.add(ul,0,0);
+		while(PosDetect<13)
+		{	
+			left[PosDetect]=new CFside(0);
+			cf.add(left[PosDetect],0,PosDetect+1);
+			PosDetect++;
+		}
+		cf.add(dl,0,14);
+		PosDetect=0;
+		for(int i=0;i<18;i++)
+			for(int j=0;j<15;j++)
+			{
+				switch(j)
+				{
+				case 0:top[i]=new CFside(1);
+						cf.add(top[i], i+1, j);
+					break;
+				case 14:
+					bottom[i]=new CFside(3);
+					cf.add(bottom[i], i+1, j);
+					break;
+				default:
+					mids[i*(j-1)]=new CFmid();
+					cf.add(mids[i*(j-1)], i+1, j);
+					break;
+				
+				}
+			}
+		cf.add(ur, 19, 0);
+		while(PosDetect<13)
+		{
+			rig[PosDetect]=new CFside(2);
+			cf.add(rig[PosDetect], 19, PosDetect+1);
+			PosDetect++;
+		}
+		cf.add(dr, 19, 14);
 	}
 }
